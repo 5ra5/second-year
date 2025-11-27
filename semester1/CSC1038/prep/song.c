ee62549634ad8986d6a75baedc7ce2d496886a2e
Petra Sartori <petra.sartori2@mail.dcu.ie>
2025-11-27 00:19:48 +0000

    Nov 27

---
+++ semester1/CSC1038/prep/song.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node{
+    char *song;
+    struct Node *next;
+}Node;
+
+// singly linked list (with strings) & dynamic memory allocation
+
+Node* buildList(int length, char*argv[]);
+void printList(Node *head);
+void freeSongs(Node *head);
+
+// get the length, initialise node *head by calling a function to build a list starting from argv[1]
+int main(int argc, char *argv[]){
+
+    int length = argc - 1;
+    Node* head = buildList(length, &argv[1]);
+    printList(head);
+    freeSongs(head);
+
+    return 0;
+}
+
+// build a singly linked list with songs (arrays of characters)
+Node* buildList(int length, char*argv[])
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    for(int i = 0; i < length; ++i)
+    {
+        // create a new node
+        Node *newNode = calloc(1, sizeof(Node));
+        if (!newNode){
+            printf("Calloc failed!\n");
+        }
+
+        // allocate memory for the song string
+        newNode->song = malloc(strlen(argv[i]) + 1);
+        strcpy(newNode->song, argv[i]);
+
+        newNode->next = NULL;
+
+        // append to list
+        if(tail){
+            tail->next = newNode;
+        }else{
+            head = newNode;
+        }
+        tail = newNode;
+    }
+
+    return head;
+}
+
+// iterate through the list and print
+void printList(Node *head){
+    Node *current = head;
+    while (current) {
+        printf("%s\n", current->song);
+        current = current->next;
+    }
+}
+
+// free the memory after using
+void freeSongs(Node *head){
+    Node *current = head;
+    while(current){
+        Node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+
+
